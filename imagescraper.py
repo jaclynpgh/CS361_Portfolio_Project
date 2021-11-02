dc05459bcb65f830c04fa3b99f9780df5b4c9289
jaclynpgh <mannja@oregonstate.edu>
2021-11-02 15:29:22 -0400

    updated to search any website

--- imagescraper.py
+++ imagescraper.py
@@ -1,35 +1,28 @@
+
 import requests
 from bs4 import BeautifulSoup
 import json
-def image_scraper(query):
-    adlt = 'moderate'
-    search = query.strip()
-    search = search.replace(' ', '+')
-    url = 'https://bing.com/images/search?q=' + search + '&safeSearch=' + adlt
-    print("\nsearch url:", url, "\n")
-    # A user agent is a computer program representing a person; https://developer.mozilla.org/en-US/docs/Glossary/User_agent
-    USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
-    headers = {"user-agent": USER_AGENT}
-    resp = requests.get(url, headers=headers)
-    soup = BeautifulSoup(resp.content, "html.parser")
-    image_dict = {}
-    # finds image links
-    images = soup.find_all('a', class_='iusc')
-    for i in images:
+
+
+def image_scraper(url):
+
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    img_tags = soup.find_all('img')
+
+    images = {}
+    for img in img_tags:
         try:
-            img_url = eval(i['m'])['murl']
-            img_title = img_url.split("/")[-1]
-            image_dict[img_title] = img_url
-           # print(img_title,':', img_url)
+            name = img['alt']
+            link = img['src']
+            images[name] = link
         except:
-
             pass
-    return json.dumps(image_dict, indent=4)
-
-
+    return json.dumps(images, indent=4)
 
 
 if __name__ == "__main__":
-    query = input("Enter a search term for your image: ")
-    results = image_scraper(query)
+    url = input("Enter a website: ")
+    results = image_scraper(url)
     print(results)
