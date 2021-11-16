5f058625de4086d2df1803f70d008f6012cca74b
jaclynpgh <mannja@oregonstate.edu>
2021-11-16 13:57:09 -0500

    added hotel api

---
+++ hoteldata.py
@@ -0,0 +1,35 @@
+
+
+api_key = "AIzaSyCw496DJw7o513U89nh2akKBnWtEbdPQ4Y"
+
+import requests
+from requests.exceptions import HTTPError
+
+
+
+def get_hotel_data(query):
+	url = "https://maps.googleapis.com/maps/api/place/textsearch/json?"
+
+	try:
+		response = requests.get(url + 'query=' + query + '&key=' + api_key)
+		response.raise_for_status()
+		# access JSON content
+		jsonResponse = response.json()
+		# grabs hotel info
+		result = jsonResponse['results']
+		hotel_data = {}
+		for i in range(len(result)):
+			name = result[i]['name']
+			rating = result[i]['rating']
+			hotel_data[name] = rating
+
+		return hotel_data
+
+
+	except HTTPError as http_err:
+		print(f'HTTP error occurred: {http_err}')
+	except Exception as err:
+		print(f'Other error occurred: {err}')
+
+#print(get_hotel_data("pittsburgh hotels"))
+
